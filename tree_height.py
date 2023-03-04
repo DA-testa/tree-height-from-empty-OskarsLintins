b0acc9b360411bd92961a9318653f2ef2bb68774
OskarsLintins <124681718+OskarsLintins@users.noreply.github.com>
2023-03-04 20:15:54 +0000

    Oskars

--- tree_height.py
+++ tree_height.py
@@ -33,7 +33,7 @@ def main():
         if filename.endswith("a"):
             return
         else:
-            with open('/workspaces/tree-height-from-empty-OskarsLintins/test/' + filename) as fn:
+            with open('/workspaces/tree-height-from-empty-OskarsLintins/test/05') as fn:
                 n = int(fn.readline())
                 parents = list(map(int, fn.readline().split()))
 
