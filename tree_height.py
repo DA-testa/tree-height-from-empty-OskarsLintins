44692c6407ed27731436e8f860e84fb176a5ee8b
OskarsLintins <124681718+OskarsLintins@users.noreply.github.com>
2023-03-05 19:15:18 +0000

    test

--- tree_height.py
+++ tree_height.py
@@ -30,7 +30,7 @@ def main():
 
     elif word[0] == "F":
         filename = input().strip()
-        filepath = os.path.join(os.getcwd(),'test/' + filename)
+        filepath = os.path.join(os.getcwd(), filename)
         if filename.endswith("a"):
             return
         else:
